332a0af21c435955b2ccace7a5c205b1f8fea137
claudine_sinclair <femtobyte@gmail.com>
2016-01-25 16:23:20 -0800

    added searchbar filtering

--- Pokemonidex/ViewController.swift
+++ Pokemonidex/ViewController.swift
@@ -9,19 +9,25 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     @IBOutlet weak var collection: UICollectionView!
-    
-    var musicPlayer: AVAudioPlayer!
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var soundBtn: UIButton!
-    
+
+    var musicPlayer: AVAudioPlayer!
+    var filteredPokemon = [Pokemon]()
     var pokemonArray = [Pokemon]()
+    var inSearchMode = false
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.delegate = self
         collection.dataSource = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done  
         parsePokemonCSV()
         initAudio()
     }
@@ -56,7 +62,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokieCell", forIndexPath: indexPath) as? PokieCell {
-            let pokemon = pokemonArray[indexPath.row]
+         //   let pokemon = pokemonArray[indexPath.row]
+            let pokemon: Pokemon!
+            
+            if inSearchMode {
+                pokemon = filteredPokemon[indexPath.row]
+            } else {
+                pokemon = pokemonArray[indexPath.row]
+            }
+            
             cell.configureCell(pokemon)
             return cell
         } else{
@@ -69,7 +83,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        if inSearchMode{
+            return filteredPokemon.count
+        }
+        return pokemonArray.count
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -84,13 +101,31 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         if musicPlayer.playing {
             musicPlayer.stop()
-            sender.alpha = 0.2
-            // how to swap out icon?
-         //   sender.setImage(MuteIcon.png, forState: false)
+            sender.alpha = 0.1
         } else {
             musicPlayer.play()
             sender.alpha = 1.0
-        //    sender.setImage(SoundIcon.png, forState: true)
+        }
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)   //dismiss keyboard
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            view.endEditing(true)   //dismiss keyboard
+            collection.reloadData()
+        } else {
+            inSearchMode = true
+            let lower = searchBar.text!.lowercaseString
+            
+            // $0 grabs an element out of array.  rangeOfString is a func that checks every element in array to find if the 
+            //  letters being passed to it exist in any of the strings in the elements of the array.  This call is built to be efficient and fast, 
+            //  you could use a for loop and check the array yourself but it wouldn't be as efficient or fast.
+            filteredPokemon = pokemonArray.filter({$0.name.rangeOfString(lower) != nil})
+            collection.reloadData()
         }
     }
 }
